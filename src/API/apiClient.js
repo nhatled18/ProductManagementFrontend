0c4af1a9802e34a04b9132559c536327be5bcb75
nhatled18 <nhatled18@gmail.com>
2025-10-24 15:38:33 +0700

    add .env

--- src/API/apiClient.js
+++ src/API/apiClient.js
@@ -1,11 +1,10 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
   headers: { 'Content-Type': 'application/json' },
 });
 
-// Thêm token vào mọi request
 apiClient.interceptors.request.use((config) => {
   const token = localStorage.getItem('authToken');
   if (token) config.headers.Authorization = `Bearer ${token}`;
